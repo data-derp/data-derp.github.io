2d44da05f6bb092b96d431fb69f44e0333d75ce2
Kelsey Mok <kelseymok@gmail.com>
2021-11-28 17:26:00 +0100

    pl/km: removing auto generated directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kmok/Projects/data-derp/data-derp.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kmok/Projects/data-derp/data-derp.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kmok/Projects/data-derp/data-derp.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/kmok/Projects/data-derp/data-derp.github.io/src/css/custom.css"),
-];
