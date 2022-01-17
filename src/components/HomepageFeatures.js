f6d88d9bc8bbca90d665fa39d07f2f7b248919db
Kelsey Mok <kelseymok@gmail.com>
2022-01-17 11:22:27 +0100

    km: passing png

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,7 +5,7 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Wide Breadth',
-    Svg: require('../../static/img/wide-breadth.png').default,
+    Png: require('../../static/img/wide-breadth.png').default,
     description: (
       <>
           Give enough context across the whole domain in order to enable deeper dive into relevant and advanced topics later
@@ -14,7 +14,7 @@ const FeatureList = [
   },
   {
     title: 'Real World Slice',
-    Svg: require('../../static/img/real-world-slice.png').default,
+    Png: require('../../static/img/real-world-slice.png').default,
     description: (
       <>
           * Storage
@@ -28,7 +28,7 @@ const FeatureList = [
   },
   {
     title: 'Real World Scenario',
-    Svg: require('../../static/img/real-world-scenario.png').default,
+    Png: require('../../static/img/real-world-scenario.png').default,
     description: (
       <>
           * Projecting and Visualising CO2 emissions vs. Global temperature
@@ -39,11 +39,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Png, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <Png className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
