c05a57a08bf6d2ffc5aff369006680ecd7a40872
Kelsey Beyer <kelseymok@gmail.com>
2023-05-02 19:43:57 +0200

    adding base url with trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Data Derp',
     // tagline: 'Data Derp',
   url: 'https://data-derp.github.io',
-  baseUrl: '/v1.0',
+  baseUrl: 'v1.0/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   // favicon: 'img/favicon.ico',
